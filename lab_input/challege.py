37ad76388b82f8048f368fcf0c8f0c9272c942c2
likun47 <likun47@gmail.com>
2023-02-21 20:43:12 +0000

    manipulating lists

---
+++ lab_input/challege.py
@@ -0,0 +1,12 @@
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""    
+
+def main():
+
+    user_input1 = input("Please enter a name: ")
+   
+    user_input2 = input("Please enter day of the week: ")
+
+    print("Hello", user_input1, "!Happy ", user_input2)
+
+main()
